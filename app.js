37c1ab1626d08755bb81797bab01c0c19fd33d8c
Subodh <subodhlst@gmail.com>
2025-08-12 20:48:05 +0530

    handle post request and respond to URL parameters

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'hello from the server side', app: 'Natours' });
+    .json({ status: 'success', result: tours.length, data: { tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'failed', message: 'invalid ID' });
+  }
+  res.status(200).json({ status: 'success', data: tour });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newtour = Object.assign({ id: newId }, req.body);
+  tours.push(newtour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tours: newtour } });
+    }
+  );
 });
 
 const port = 3000;
